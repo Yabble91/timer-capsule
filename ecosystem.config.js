bc2e7067bd3fa14166fccec1bc3e1c927b326dce
刘燕保 <liuyanbao@9fbank.com.cn>
2019-04-30 21:00:25 +0800

    增加pm2代码部署功能

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "app": [
+    {
+      "name": "timer-capsule",
+      "script": "app.js",
+      "env": {
+        "COMMON_VARIABLE": "true"
+      },
+      "env_production": {
+        "NODE_ENV": "production"
+      }
+    }
+  ],
+  "deploy": {
+    "aliTimerCapsule": {
+      "user": "root",
+      "host": ["47.93.15.178"],
+      "ref": "origin/master",
+      "repo": "https://github.com/Yubble/timer-capsule.git",
+      "path": "/www/website/timerCapsule",
+      "ssh_options": "StrictHostKeyChecking=no",
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+}
