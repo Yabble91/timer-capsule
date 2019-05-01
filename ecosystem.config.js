a24f2c1e137c264e225baaa707f4b37535ebf5ca
刘燕保 <liuyanbao@9fbank.com.cn>
2019-05-01 10:31:46 +0800

    让pm2部署

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       "repo": "https://github.com/Yubble/timer-capsule.git",
       "path": "/www/website/timerCapsule",
       "ssh_options": "StrictHostKeyChecking=no",
-      "post-deploy":"yarn install",
+      "post-deploy":"yarn install && node pre",
       "env": {
         "NODE_ENV": "production"
       }
